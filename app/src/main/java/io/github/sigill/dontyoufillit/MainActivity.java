72f1e905414b291624848f3a25196f2b0e1f5fce
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2018-03-03 14:02:20 +0100

    Enable js, localstorage and make logcat display messages from thee js console

--- app/src/main/java/io/github/sigill/dontyoufillit/MainActivity.java
+++ app/src/main/java/io/github/sigill/dontyoufillit/MainActivity.java
@@ -2,6 +2,10 @@ package io.github.sigill.dontyoufillit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import static android.view.WindowManager.*;
@@ -19,6 +23,18 @@ public class MainActivity extends Activity {
         this.getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN);
 
         mWebView = findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public boolean onConsoleMessage(ConsoleMessage cm) {
+                Log.d("DontYouFillIt", cm.message()
+                        + " -- From line " + cm.lineNumber()
+                        + " of " + cm.sourceId() );
+                return true;
+            }
+        });
 
         mWebView.loadUrl("file:///android_asset/index.html");
     }
