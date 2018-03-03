9292d4d6edb3e4be5c4a0cb53d1159873eaa3f5b
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2018-03-03 13:39:17 +0100

    Make the app fullscreen (hide the status bar)

--- app/src/main/java/io/github/sigill/dontyoufillit/MainActivity.java
+++ app/src/main/java/io/github/sigill/dontyoufillit/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
+import static android.view.WindowManager.*;
+
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hide the status bar.
+        this.getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN);
 
         mWebView = findViewById(R.id.activity_main_webview);
 
