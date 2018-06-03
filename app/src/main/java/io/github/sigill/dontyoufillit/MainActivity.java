240c87c9f7248a9abcb6e1270aa07df63b8610f4
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2018-06-03 10:03:16 +0200

    Display version number on main screen

--- app/src/main/java/io/github/sigill/dontyoufillit/MainActivity.java
+++ app/src/main/java/io/github/sigill/dontyoufillit/MainActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView versionView = findViewById(R.id.mainVersion);
+        versionView.setText(BuildConfig.VERSION_NAME);
     }
 
     public void launchGame(View v) {
